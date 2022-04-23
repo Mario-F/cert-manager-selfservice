3e065ebdcfd311ee4b4e45c46d7574a95a844d90
Mario-F <github@fritschen.net>
2022-04-23 15:01:49 +0200

    feat(api): implment placeholder for get status
    Signed-off-by: Mario-F <github@fritschen.net>

--- internal/server/handlers/status.go
+++ internal/server/handlers/status.go
@@ -2,12 +2,19 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/Mario-F/cert-manager-selfservice/internal/gen/api"
 	"github.com/labstack/echo/v4"
-	log "github.com/sirupsen/logrus"
 )
 
 func (h *OpenAPIV1HandlerImpl) GetStatus(ctx echo.Context) error {
-	log.Debug("GetStatus")
-	return ctx.JSON(http.StatusOK, "OK")
+	res := &api.Status{}
+	// TODO: Fill with real data
+	res.Messages = append(res.Messages, api.StatusMessage{
+		Message:  "This is a placeholder message for now",
+		Severity: "info",
+		Time:     time.Now(),
+	})
+	return ctx.JSON(http.StatusOK, res)
 }
