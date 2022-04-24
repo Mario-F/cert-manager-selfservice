6a01045e0db6c879c54580229ba78357f6071dba
Mario-F <github@fritschen.net>
2022-04-24 20:41:14 +0200

    feat(web): added coreui
    Signed-off-by: Mario-F <github@fritschen.net>

--- vetur.config.js
+++
@@ -1,6 +0,0 @@
-// vetur.config.js
-module.exports = {
-  projects: [
-    './web',
-  ],
-}
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@coreui/coreui/dist/css/coreui.min.css'
 
 import App from './App.vue'
 import router from './router'
