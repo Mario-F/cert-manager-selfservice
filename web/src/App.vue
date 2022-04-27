6ee6722e05b0eb9f38c7eeab3c7b8df41c389c81
Mario-F <github@fritschen.net>
2022-04-27 17:18:36 +0200

    feat(web): try integrate coreui
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import { CSidebar, CSidebarBrand, CSidebarNav, CNavItem, CBadge, CNavGroup, CSidebarToggler } from '@coreui/vue'
+import {
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CNavItem,
+  CBadge,
+  CNavGroup,
+  CSidebarToggler,
+  CContainer,
+  CHeader,
+  CHeaderBrand,
+  CHeaderToggler,
+  CCollapse,
+  CHeaderNav,
+  CNavLink,
+  CDropdown,
+  CDropdownItem,
+} from '@coreui/vue'
 import { CIcon } from '@coreui/icons-vue'
 import * as icons from '@coreui/icons/js/free'
+
+let visible = true
 </script>
 
 <template>
@@ -11,15 +30,19 @@ import * as icons from '@coreui/icons/js/free'
       <CSidebarBrand>Sidebar Brand</CSidebarBrand>
       <CSidebarNav>
         <li class="nav-title">Nav Title</li>
-        <CNavItem href="#">
-          <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
-          Nav item
-        </CNavItem>
-        <CNavItem href="#">
-          <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
-          With badge
-          <CBadge color="primary" class="ms-auto">NEW</CBadge>
-        </CNavItem>
+        <RouterLink to="/">
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
+            Home
+          </CNavItem>
+        </RouterLink>
+        <RouterLink to="/about">
+          <CNavItem href="#">
+            <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
+            About
+            <CBadge color="primary" class="ms-auto">NEW</CBadge>
+          </CNavItem>
+        </RouterLink>
         <CNavGroup>
           <template #togglerContent>
             <CIcon customClassName="nav-icon" :icon="icons.cilPuzzle" /> Nav dropdown
@@ -31,13 +54,42 @@ import * as icons from '@coreui/icons/js/free'
       <CSidebarToggler />
     </CSidebar>
 
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+      <CHeader>
+        <CContainer fluid>
+          <CHeaderBrand href="#">Header</CHeaderBrand>
+          <CHeaderToggler @click="visible = !visible" />
+          <CCollapse class="header-collapse" :visible="visible">
+            <CHeaderNav>
+              <CNavItem>
+                <CNavLink href="#" active> Home </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink href="#">Link</CNavLink>
+              </CNavItem>
+              <CDropdown togglerText="Dropdown" variant="nav-item">
+                <CDropdownItem href="#">Action</CDropdownItem>
+                <CDropdownItem href="#">Another action</CDropdownItem>
+                <CDropdownItem href="#">Something else here</CDropdownItem>
+              </CDropdown>
+              <CNavItem>
+                <CNavLink href="#" disabled> Disabled </CNavLink>
+              </CNavItem>
+            </CHeaderNav>
+            <!-- <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form> -->
+          </CCollapse>
+        </CContainer>
+      </CHeader>
+      <div class="body flex-grow-1 px-3">
+        <CContainer lg>
+          <RouterView />
+        </CContainer>
+      </div>
+    </div>
   </div>
-
-  <RouterView />
 </template>
 
 <style></style>
