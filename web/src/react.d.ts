bdf7fd02a305089b0b955bad6bce8f3fa0e92dd4
Mario-F <github@fritschen.net>
2022-05-01 23:02:26 +0200

    fix(web): workaround types other way
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/src/react.d.ts
+++
@@ -1,9 +0,0 @@
-// Workaround: https://github.com/johnsoncodehk/volar/discussions/592
-import { AriaAttributes, DOMAttributes } from 'react' // not needed if skipLibCheck = true
-
-declare module 'react' {
-  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    // extends React's HTMLAttributes
-    class?: string
-  }
-}
