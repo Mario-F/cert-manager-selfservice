3679b0a58c30bba43b0c3aaa4f97d9f2d35db8ad
Mario-F <github@fritschen.net>
2022-05-01 13:14:19 +0200

    build(web): added vite proxy
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8030',
+        changeOrigin: true,
+      },
+    },
+  },
 })
