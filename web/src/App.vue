6b687532dcd42f3f46cf8283ed043a01e18c76a7
Mario-F <github@fritschen.net>
2022-05-01 22:26:46 +0200

    feat(web): cleanup examples
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,97 +1,44 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import {
-  CSidebar,
-  CSidebarBrand,
-  CSidebarNav,
-  CNavItem,
-  CBadge,
-  CNavGroup,
-  CSidebarToggler,
-  CContainer,
-  CHeader,
-  CHeaderBrand,
-  CHeaderNav,
-  CDropdownMenu,
-  CDropdownToggle,
-  CNavLink,
-  CDropdown,
-  CDropdownItem,
-} from '@coreui/vue'
+import { CSidebar, CSidebarBrand, CSidebarNav, CNavItem, CBadge, CSidebarToggler } from '@coreui/vue'
 import { CIcon } from '@coreui/icons-vue'
-import * as icons from '@coreui/icons/js/free'
+import * as icons from '@coreui/icons'
 import { ref } from 'vue'
 
 const sidebar = {
-  visible: ref(true),
   unfoldable: ref(false),
 }
 </script>
 
 <template>
-  <CSidebar position="fixed" :unfoldable="sidebar.unfoldable.value" :visible="sidebar.visible.value">
-    <CSidebarBrand>Sidebar Brand</CSidebarBrand>
+  <CSidebar position="fixed" :unfoldable="sidebar.unfoldable.value" visible>
+    <CSidebarBrand>Cert Manager Selfservice</CSidebarBrand>
     <CSidebarNav>
-      <li class="nav-title">Nav Title</li>
       <RouterLink to="/">
         <CNavItem href="#">
           <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
-          Home
-        </CNavItem>
-      </RouterLink>
-      <RouterLink to="/about">
-        <CNavItem href="#">
-          <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
-          About
-          <CBadge color="primary" class="ms-auto">NEW</CBadge>
+          Dashboard
         </CNavItem>
       </RouterLink>
       <RouterLink to="/swagger">
         <CNavItem href="#">
-          <CIcon customClassName="nav-icon" :icon="icons.cilSpeedometer" />
+          <CIcon customClassName="nav-icon" :icon="icons.cibSwagger" />
           API
           <CBadge color="primary" class="ms-auto">Swagger</CBadge>
         </CNavItem>
       </RouterLink>
-      <CNavGroup>
-        <template #togglerContent> <CIcon customClassName="nav-icon" :icon="icons.cilPuzzle" /> Nav dropdown </template>
-        <CNavItem href="#"> <CIcon customClassName="nav-icon" :icon="icons.cilPuzzle" /> Nav dropdown item </CNavItem>
-        <CNavItem href="#"> <CIcon customClassName="nav-icon" :icon="icons.cilPuzzle" /> Nav dropdown item </CNavItem>
-      </CNavGroup>
+      <RouterLink to="/about">
+        <CNavItem href="#">
+          <CIcon customClassName="nav-icon" :icon="icons.cilInfo" />
+          About
+        </CNavItem>
+      </RouterLink>
     </CSidebarNav>
     <CSidebarToggler class="d-none d-lg-flex" @click="sidebar.unfoldable.value = !sidebar.unfoldable.value" />
   </CSidebar>
 
   <div class="wrapper d-flex flex-column min-vh-100 bg-light">
-    <CHeader>
-      <CContainer fluid>
-        <CHeaderBrand href="#">Header</CHeaderBrand>
-        <CHeaderNav variant="pills">
-          <CNavItem>
-            <RouterLink to="/"><CNavLink>Home</CNavLink></RouterLink>
-          </CNavItem>
-          <CNavItem>
-            <RouterLink to="/about"><CNavLink>About</CNavLink></RouterLink>
-          </CNavItem>
-          <CDropdown togglerText="Dropdown" variant="nav-item">
-            <CDropdownToggle color="primary">Dropdown Button</CDropdownToggle>
-            <CDropdownMenu>
-              <CDropdownItem href="#">Action</CDropdownItem>
-              <CDropdownItem href="#">Another action</CDropdownItem>
-              <CDropdownItem href="#">Something else here</CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-          <CNavItem>
-            <CNavLink href="#" disabled> Disabled </CNavLink>
-          </CNavItem>
-        </CHeaderNav>
-      </CContainer>
-    </CHeader>
-    <div class="body flex-grow-1 px-3">
-      <CContainer lg>
-        <RouterView />
-      </CContainer>
-    </div>
+    <RouterView />
   </div>
 </template>
 
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,7 +23,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Swagger.vue'),
+      component: () => import('../views/SwaggerView.vue'),
     },
   ],
 })
