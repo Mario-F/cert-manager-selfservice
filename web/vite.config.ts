bdc8f131bdb2e3ae628ed1b0f8d3c51f43f7578f
Mario-F <github@fritschen.net>
2022-05-02 18:29:52 +0200

    build(web): increase chunk size warning
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1024 * 1024 * 2,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
