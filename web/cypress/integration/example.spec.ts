4e8c39d449db94d23ab0da683051ce51713a4636
Mario-F <github@fritschen.net>
2022-05-08 11:40:14 +0200

    test(web): updated cypress example
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/cypress/integration/example.spec.ts
+++ web/cypress/integration/example.spec.ts
@@ -2,7 +2,13 @@
 
 describe('My First Test', () => {
   it('visits the app root url', () => {
+    cy.intercept('GET', '/api/v1/info', {
+      statusCode: 200,
+      body: {
+        version: 'integration-test',
+      },
+    })
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('div', 'Cert Manager Selfservice')
   })
 })
