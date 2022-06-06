c58ad901b51955228f05860631db55fe110c1c78
Mario-F <github@fritschen.net>
2022-06-06 18:50:26 +0200

    feat(web): added hint to dashboard view
    Signed-off-by: Mario-F <github@fritschen.net>

--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import { CContainer, CRow, CCol, CCard, CCardBody, CCardTitle, CCardText, CHeaderBrand } from '@coreui/vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <CContainer fluid>
+    <CRow>
+      <CHeaderBrand>Dashbaord</CHeaderBrand>
+    </CRow>
+    <CRow>
+      <CCol>
+        <CCard>
+          <CCardBody>
+            This should become a dashboard view, for now this is on TODO check out API swagger docs for usage.
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </CContainer>
 </template>
